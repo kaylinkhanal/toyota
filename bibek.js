3ce4e681d5dcfef4253ccabf112f4d8337a45445
bibek-404 <bhattabibek2004@gmail.com>
2025-06-05 17:57:02 +0545

    added object code

--- bibek.js
+++ bibek.js
@@ -1,3 +1,9 @@
-const obj = {
-name: "bibek"
-}
+const person = {
+  firstName: "Bibek",
+  lastName: "Bhatta",
+  id: 5566,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+ }
+};
+console.log(person)
