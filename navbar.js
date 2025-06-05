4ea3ed12a5ff240cc6ff2812074aecf1990c34ce
Prabin <prabinkhokhali89@gmail.com>
2025-06-05 07:40:08 +0545

    added navbar

---
+++ navbar.js
@@ -0,0 +1,19 @@
+const obj = {
+  "navbar": {
+    "title": "My Website",
+    "links": [
+      {
+        "name": "Home",
+        "url": "/home"
+      },
+      {
+        "name": "About",
+        "url": "/about"
+      },
+      {
+        "name": "Contact",
+        "url": "/contact"
+      }
+    ]
+  }
+}
