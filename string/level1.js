c743db7eccf3fc56bf3a394462c70d9d4ec981c6
Abhisekh Kumar Aman <abhisekh@getblys.com>
2025-06-06 07:49:04 +0545

    Completed the string level 1 assignment

--- string/level1.js
+++ string/level1.js
@@ -1,9 +1,12 @@
 const name = 'rameshwor'
 
 //Q1. output should be ram
-
+const output1 = name.slice(0, 3);
+console.log(output1)
 
 //Q2 output should be ['r','a','m','e','s','h','w','o', 'r']
-
+const output2 = name.split('');
+console.log(output2)
 
 // Q3 output should be 9
+console.log(name.length);
