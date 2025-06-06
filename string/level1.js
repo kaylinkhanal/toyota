16cfde0c74ac8273fca242d1daf7bea108caca08
Abhiyan Paudel <abhiyanpaudel@Abhiyans-MacBook-Air.local>
2025-06-06 07:46:44 +0545

    Completed String Level 1

--- string/level1.js
+++ string/level1.js
@@ -1,9 +1,12 @@
 const name = 'rameshwor'
 
 //Q1. output should be ram
-
+let sliced= name.slice(0,3)
+console.log(sliced)
 
 //Q2 output should be ['r','a','m','e','s','h','w','o', 'r']
-
+let newArray= name.split("")
+console.log(newArray)
 
 // Q3 output should be 9
+console.log(name.length)
