be33d98780763fa580b2f5b78822130ca20afb94
Rajil <shrestharajill11@gmail.com>
2025-06-06 07:51:03 +0545

    solved q2

--- string/level1.js
+++ string/level1.js
@@ -1,10 +1,13 @@
 const name = 'rameshwor'
 
 //Q1. output should be ram
-
+console.log(name.slice(0, 3))
 
 
 //Q2 output should be ['r','a','m','e','s','h','w','o', 'r']
+
+console.log(name.split(''))
+
 
 
 // Q3 output should be 9
