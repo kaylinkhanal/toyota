237c168305253c259a45aacc90ec4dd65adc9c23
Deeparshan <deeparshanpro@gmail.com>
2025-06-06 07:46:17 +0545

    Solved all the answers

--- string/level1.js
+++ string/level1.js
@@ -1,9 +1,13 @@
 const name = 'rameshwor'
 
 //Q1. output should be ram
+let sliced = name.slice(0,3)
+console.log(sliced)
 
 
 //Q2 output should be ['r','a','m','e','s','h','w','o', 'r']
-
+let toArray = name.split("")
+console.log(toArray)
 
 // Q3 output should be 9
+console.log(name.length)
