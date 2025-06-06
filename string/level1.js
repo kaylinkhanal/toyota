5727692c3033d1b2c16a4dcfb860f9f2344797e2
Aadarsha Khadka <meaadar1@gmail.com>
2025-06-06 08:17:34 +0545

    solution string/level1.js

--- string/level1.js
+++ string/level1.js
@@ -1,8 +1,13 @@
 const name = 'rameshwor'
 
 //Q1. output should be ram
-
+let slicedName = name.slice(0, 3);
+console.log(slicedName);
 
 //Q2 output should be ['r','a','m','e','s','h','w','o', 'r']
 
+let arrName = name.split('');
+console.log(arrName);
+
 // Q3 output should be 9
+console.log(name.length);
