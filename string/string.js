4b4a3e9d9f91e326e877ce835183db7ba4bf4404
Naren Maharjan <naren.14151415@gmail.com>
2025-06-08 18:34:27 +0545

    assignment 1 string

---
+++ string/string.js
@@ -0,0 +1,13 @@
+const name ='rameshwwor';
+
+// output should be ram
+let no1 = name.slice(0,3);
+console.log(no1);
+
+//output should be ['r','a','m','e','s','h','w','o','r']
+
+const no2 = name.split('');
+console.log(no2);
+
+//output should be 9
+console.log(name.length);
