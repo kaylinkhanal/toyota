39aa3b046159866a12d88ddc0a7d9401676443d3
Prabin <prabinkhokhali89@gmail.com>
2025-06-08 10:41:24 +0545

    Level 5 Answer Added

--- string/level1.js
+++ string/level1.js
@@ -1,8 +1,11 @@
 const name = 'rameshwor'
 
 //Q1. output should be ram
-
+console.log(name.slice(0, 3))
 
 //Q2 output should be ['r','a','m','e','s','h','w','o', 'r']
+array = [...name];
+console.log(array)
 
 // Q3 output should be 9
+console.log(name.length)
--- string/level5.js
+++ string/level5.js
@@ -1,7 +1,15 @@
 const name  = 'ram'
 let surname = 'thapa'
 
-//expected output 
+
+
+
+output = {
+  [name]: surname
+} 
+console.log(output)
+// expected output    
 // {
 // 'ram': 'thapa'
 // }
+
