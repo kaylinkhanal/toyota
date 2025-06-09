5d6e987453932b49179ec0b84eb40a1425b832de
Rajil <shrestharajill11@gmail.com>
2025-06-09 07:34:54 +0545

    lvl5

--- object/level5.js
+++ object/level5.js
@@ -12,5 +12,10 @@ const obj = {
     // name: "John",
     // status: "distinction"
 // }
-
+if (obj<=80) {
+    obj.status ="distinction"
+}
+else {
+    obj.status = "first division"
+}
 console.log(obj)
