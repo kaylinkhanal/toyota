baeb33edb69559bfa8aa496907683c9f32dc5ccb
Naren Maharjan <naren.14151415@gmail.com>
2025-06-09 07:34:00 +0545

    array level.js

--- object/level5.js
+++ object/level5.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const obj ={
     scores:90,
     name:'john',
@@ -7,3 +8,21 @@ if (obj.scores > 80){
    obj.parandas = "very good"
    console.log("distinct");
 }
+=======
+const obj = {
+    scores: 90,
+    name: "John",
+}
+
+
+// if score is greater than 80, add new item to obj, status: 'distinction', else
+// status should be first division
+// expected output :
+// {
+    // scores: 90,
+    // name: "John",
+    // status: "distinction"
+// }
+
+console.log(obj)
+>>>>>>> 6562f344f3f8a9d75f4e0451d399efc9c89050e1
