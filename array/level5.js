2353598d45d4ae518a16bc62fcd0772654ab5933
Deeparshan <deeparshanpro@gmail.com>
2025-06-09 07:13:19 +0545

    level5 solved

--- array/level5.js
+++ array/level5.js
@@ -3,4 +3,8 @@ const arr = [1,4,5,5]
 //Q1. Check if array consists number 4, if yes, then remove that item 
 // form the array.
 
-
+if (arr.includes(4)) {
+    const index = arr.indexOf(4)
+    arr.splice(index , 1)
+    console.log(arr)
+}
