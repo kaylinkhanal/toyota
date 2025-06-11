bf15dab9fe7468b2057a0713f22a0cfce93a2986
dharmendra <[karkidharmendra21@gmail.com]>
2025-06-11 08:03:53 +0545

    level8

---
+++ array/level7.js
@@ -0,0 +1,17 @@
+
+const arr = [3,5,2,5,12,4]
+//q1. return only even numbers
+let output = arr.filter((item)=>{
+    if ( item % 2==0)
+    return item
+//return item %2==0
+})
+console.log(output)
+
+//q2. add 1 to each numbers if its divisible by 5
+let output2 = arr.map((item)=>{
+    if(item %5 ==0)
+    return item +1;
+})
+
+console.log(output2)
---
+++ array/level8.js
@@ -0,0 +1,32 @@
+const arr = [
+    {
+      "productId": "PROD-A001",
+      "name": "Wireless Bluetooth Headphones",
+      "price": 59.99,
+      "quantity": 2,
+      "subtotal": 119.98
+    },
+    {
+      "productId": "PROD-B002",
+      "name": "Ergonomic Office Chair",
+      "price": 249.00,
+      "quantity": 1,
+      "subtotal": 249.00
+    },
+    {
+      "productId": "PROD-C003",
+      "name": "USB-C Fast Charger",
+      "price": 19.50,
+      "quantity": 3,
+      "subtotal": 58.50
+    }
+  ]
+
+  // calculate the total cost of all items
+let totalSum =  0
+
+arr.forEach((item)=>{
+    totalSum= totalSum+(item.subtotal);
+   // totalSum ma harek item.price ra quantity compare garera rakhna parne cha
+})
+console.log(totalSum)
