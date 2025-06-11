29b5897132d131e6adce79713dc0c0f76102b0c2
Kaylin Khanal <kaylinkhanal@Kaylins-MacBook-Pro-2.local>
2025-06-11 07:34:56 +0545

    Added level7

--- array/level5.js
+++ array/level5.js
@@ -3,4 +3,12 @@ const arr = [1,4,5,5]
 //Q1. Check if array consists number 4, if yes, then remove that item 
 // form the array.
 
+//hint:
+// indexOf
+// remove that index from array (splice)
 
+const matchId = arr.indexOf(7)
+if(matchId !== -1){
+    arr.splice(matchId,1)
+}
+console.log(arr)
