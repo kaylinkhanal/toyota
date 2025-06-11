dd733c50f03ab3a9eb1b6a9b70fb7322d111ba58
Aadarsha Khadka <meaadar1@gmail.com>
2025-06-11 07:42:14 +0545

    level7 solution to HOF

--- array/level7.js
+++ array/level7.js
@@ -1,15 +1,17 @@
 const arr = [3,5,2,5,12,4]
 //q1. return only even numbers
 const output = arr.map((item)=>{
-    //write if condition here
-    return item
+    if(item % 2 == 0) 
+        {return item;}
+    // return item % 2 == 0;
+    
 })
 console.log(output)
 
 //q2. add 1 to each numbers if its divisible by 5
 const output2 = arr.map((item)=>{
-    //write if condition here
-    return item
+    if(item % 5 == 0)
+    return item + 1;
 })
 
 console.log(output2)
