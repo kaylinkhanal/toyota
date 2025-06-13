153456128b534b903c658977d0421bd0c4c6573c
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-06-13 07:25:37 +0545

    level7.js solution

--- object/level7.js
+++ object/level7.js
@@ -1,23 +1,40 @@
 const user = [
-    {
+  {
     name: "John Doe",
     age: 30,
     scores: {
-        math: 85,
-        english: 90,
-        science: 78
-    }
-},
-{
+      math: 85,
+      english: 90,
+      science: 78,
+    },
+  },
+  {
     name: "ram thapa",
     age: 30,
     scores: {
-        math: 82,
-        english: 94,
-        science: 71
-    }
-}
-
-]
+      math: 82,
+      english: 94,
+      science: 71,
+    },
+  },
+];
 
 /// calcualte the average percentage scored by all the students
+
+let totalPercentage = 0;
+
+user.forEach((item) => {
+  const eachScores = Object.values(item.scores);
+
+  const scores = eachScores.reduce((acc, CurrentValue) => {
+    return acc + CurrentValue;
+  });
+
+  console.log(eachScores);
+  console.log(scores);
+  totalPercentage = totalPercentage + (scores * 100) / 300;
+});
+
+console.log(totalPercentage);
+
+console.log(`Average Percentage = ${totalPercentage / user.length}`);
