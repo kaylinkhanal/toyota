c349b75cb032b00d4c71a1d6a5e6249241c8c485
bibek-404 <bhattabibek2004@gmail.com>
2025-06-17 16:49:15 +0545

    Update level5.js

--- string/level5.js
+++ string/level5.js
@@ -1,24 +1,11 @@
-const name  = 'ram'
-let surname = 'thapa'
+const name  = 'ram';
+let surname = 'thapa';
 
-<<<<<<< HEAD
-//expected output 
-// {
-// 'ram': 'thapa'
-// }
+// Expected output:
+// { 'ram': 'thapa' }
 
 const obj = {
-  [name] : surname
+  [name]: surname
 };
-console.log(obj);
-=======
-let obj = {
-    [name]: surname
-}
 
-console.log(obj)
-//expected output 
-// {
-// 'ram': 'thapa'
-// }
->>>>>>> 88b117fe4cffe8e265735bcddbad9180d19d95e2
+console.log(obj);
