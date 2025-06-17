965e76be38f4c115859998fe033e1512765b142f
bibek-404 <bhattabibek2004@gmail.com>
2025-06-17 16:49:52 +0545

    Update level6.js

--- string/level6.js
+++ string/level6.js
@@ -1,3 +1,10 @@
-const char = 'aabsshdfowakaaaalswwwl'
+const char = 'aabsshdfowakaaaalswwwl';
 
-//output should be 7
+let max = 0, count = 1;
+
+for (let i = 1; i < char.length; i++) {
+  count = (char[i] === char[i - 1]) ? count + 1 : 1;
+  if (count > max) max = count;
+}
+
+console.log(max);
