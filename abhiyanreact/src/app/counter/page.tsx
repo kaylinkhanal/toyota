78a7101fe040c6552914e31694660a4292191939
Abhiyan Paudel <abhiyanpaudel@Abhiyans-MacBook-Air.local>
2025-06-22 07:39:01 +0545

    State 1 added

--- abhiyanreact/src/app/counter/page.tsx
+++ abhiyanreact/src/app/counter/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 import React, { useState } from 'react'
 
 const Counter = () => {
-    let [a, setA] = useState(5)
+    let [a, setA] = useState(0)
     const decrement = (() => {
         if(!(a<=0)){
             setA(a-1)
@@ -14,11 +14,16 @@ const Counter = () => {
         if (!(a>=10))
         setA(a+1)
     })
+
+    const reset= (()=>{
+        setA(0)
+    })
     return (
         <div>
             {a} <br />
             <Button className='m-2 cursor-pointer' onClick={increment}>Increment</Button>
             <Button className='cursor-pointer' onClick={decrement}>Decrement</Button>
+            <Button className='cursor-pointer m-2'>Reset</Button>
         </div>
     )
 }
